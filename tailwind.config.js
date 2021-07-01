630eec9c3fffca39d33e96b2a54e8551fb6c96bd
targusrock <targusrock@gmail.com>
2021-07-01 22:24:55 +0530

    Add tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        epilogue: ['Epilogue', 'sans-serif'],
+      },
+      fontWeight: {
+        black: 900,
+      },
+      boxShadow: {
+        hoverShadow: '0px 7px 29px rgba(100, 100, 111, 0.2)',
+      },
+      dropShadow: {
+        soft: 'drop-shadow(1px 3px 4px rgba(238, 238, 238, 0.8))',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
