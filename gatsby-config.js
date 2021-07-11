8166a2b4ea9d695171173637158637888817afd8
targusrock <68328137+targusrock@users.noreply.github.com>
2021-07-11 17:04:54 +0000

    Deploying to main from @ targusrock/Portfolio-Website@fc06da3cabba8f04699a98bdd57870f94229b17c 🚀

--- gatsby-config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
-
-module.exports = {
-  plugins: ['gatsby-plugin-postcss', 'gatsby-plugin-dark-mode', 'gatsby-plugin-react-helmet'],
-};
--- src/components/__tests__/Footer.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Footer from '../Footer';
-
-test('Checks year in <Footer /> text', () => {
-  const { getByTestId } = render(<Footer />);
-
-  expect(getByTestId(/footer-text/)).toHaveTextContent('in 2021');
-});
