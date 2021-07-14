510de97c676b1e14a5c52c9e68cc279dce591270
targusrock <targusrock@gmail.com>
2021-07-14 10:52:30 +0530

    Fixed theme toggle and responsiveness of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import { ThemeToggler } from 'gatsby-plugin-dark-mode';
 import * as di from 'react-icons/di';
 import * as hi from 'react-icons/hi';
 
 function Navbar() {
-  // const isBrowser = typeof window !== 'undefined';
-  const [isOpen, setIsOpen] = useState(true);
-  // const [isOpen, setIsOpen] = useState(() => {
-  //   if (isBrowser) {
-  //     return window.innerWidth > 1024 ? true : false;
-  //   }
-  // });
+  const [isOpen, setIsOpen] = useState(false);
   const activeItem = 'border-b-2 border-indigo-500';
 
-  useEffect(() => {
-    const hideMenu = () => {
-      if (window.innerWidth < 1024) {
-        setIsOpen(false);
-      } else {
-        setIsOpen(true);
-      }
-    };
-    window.addEventListener('resize', hideMenu);
-
-    return () => {
-      window.removeEventListener('resize', hideMenu);
-    };
-  }, []);
+  const navlinks = {
+    Home: '/',
+    Social: '/social',
+    Projects: '/projects',
+    About: '/about',
+  };
 
   const toggle = () => {
     setIsOpen(!isOpen);
@@ -37,9 +23,12 @@ function Navbar() {
     <>
       <nav className="lg:h-24 lg:px-16 px-4 py-3 lg:flex lg:justify-between items-center flex-shrink-0">
         <div className="flex justify-between w-full" data-testid="navbar-icons">
-          <ThemeToggler role="themeToggler">
-            {({ theme, toggleTheme }) =>
-              theme === 'light' ? (
+          <ThemeToggler>
+            {({ theme, toggleTheme }) => {
+              if (theme == null) {
+                return null;
+              }
+              return theme === 'light' ? (
                 <hi.HiOutlineSun
                   className="w-8 h-8 mb-1 text-current cursor-pointer"
                   onClick={() => toggleTheme('dark')}
@@ -49,8 +38,8 @@ function Navbar() {
                   className="w-8 h-8 mb-1 text-current cursor-pointer"
                   onClick={() => toggleTheme('light')}
                 />
-              )
-            }
+              );
+            }}
           </ThemeToggler>
           <button
             onClick={toggle}
@@ -58,51 +47,39 @@ function Navbar() {
             data-testid="menuButton"
           >
             <svg fill="currentColor" viewBox="0 0 20 20" className="w-6 h-6 mb-1">
-              <path
-                fill-rule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                clip-rule="evenodd"
-                className={isOpen ? 'hidden' : 'w-8 h-8'}
-              ></path>
-              <path
-                fill-rule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-                className={isOpen ? 'w-8 h-8 mb-1' : 'hidden'}
-              ></path>
+              {isOpen ? (
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                  className={'w-8 h-8 mb-1'}
+                ></path>
+              ) : (
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                  className={'w-8 h-8'}
+                ></path>
+              )}
             </svg>
           </button>
         </div>
-        <div
-          className={isOpen ? 'lg:mt-0 mt-5' : 'hidden'}
-          onClick={!toggle}
-          onKeyDown={!toggle}
-          role="presentation"
-        >
+        <div className={`lg:block ${isOpen ? '' : 'hidden'}`}>
           <ul
             className="flex lg:justify-center justify-evenly items-center font-light text-lg"
             data-testid="navbar-navlinks"
           >
-            <li className="lg:mr-10 border-b-2 border-transparent">
-              <Link to="/" activeClassName={activeItem} className="pb-1">
-                Home
-              </Link>
-            </li>
-            <li className="lg:mr-10 border-b-2 border-transparent">
-              <Link to="/social" activeClassName={activeItem} className="pb-1">
-                Social
-              </Link>
-            </li>
-            <li className="lg:mr-10 border-b-2 border-transparent">
-              <Link to="/projects" activeClassName={activeItem} className="pb-1">
-                Projects
-              </Link>
-            </li>
-            <li className="lg:mr-10 border-b-2 border-transparent">
-              <Link to="/about" activeClassName={activeItem} className="pb-1">
-                About
-              </Link>
-            </li>
+            {Object.keys(navlinks).map((link) => {
+              return (
+                <li className="lg:mr-10 border-b-2 border-transparent">
+                  <Link to={navlinks[link]} activeClassName={activeItem} className="pb-1">
+                    {link}
+                  </Link>
+                </li>
+              );
+            })}
+
             <li>
               <a href="https://www.github.com/targusrock" target="_blank" rel="noreferrer">
                 <di.DiGithubBadge className="h-8 w-8 md:mb-1 mb-2 text-gray-400 dark:text-gray-50" />
