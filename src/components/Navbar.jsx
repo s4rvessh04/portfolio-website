25d114bce590d9963466e6463b53cf840db3af2f
targusrock <targusrock@gmail.com>
2021-10-31 20:56:20 +0530

    Refactored

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,9 @@ function Navbar() {
 
   const navlinks = {
     Home: '/',
-    Social: '/social',
-    Projects: '/projects',
-    About: '/about',
+    Social: '/social/',
+    Projects: '/projects/',
+    About: '/about/',
   };
 
   const toggle = () => {
@@ -51,16 +51,16 @@ function Navbar() {
             <svg fill="currentColor" viewBox="0 0 20 20" className="w-6 h-6 mb-1">
               {isOpen ? (
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                   className={'w-8 h-8 mb-1'}
                 ></path>
               ) : (
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                   className={'w-8 h-8'}
                 ></path>
               )}
@@ -82,7 +82,7 @@ function Navbar() {
               );
             })}
 
-            <li>
+            <li key="github-link">
               <a href="https://www.github.com/targusrock" target="_blank" rel="noreferrer">
                 <di.DiGithubBadge className="h-8 w-8 md:mb-1 mb-2 text-gray-400 dark:text-gray-50" />
               </a>
